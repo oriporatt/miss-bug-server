e861637010068485400e5159c89005cad17a94c0
oriporatt <oriporatt@gmail.com>
2025-02-10 15:52:13 +0200

    Update project bugs server with users

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getUser, getUsers, deleteUser, updateUser,addUser } from './user.controller.js'
-//test
+
 const router = express.Router()
 
 router.get('/', getUsers)
