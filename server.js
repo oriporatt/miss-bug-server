99e479b847390508c50a34d835377880cde4caaf
oriporatt <oriporatt@gmail.com>
2025-02-17 22:42:01 +0200

    uploaded port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/**', (req, res) => {
 })
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
 	loggerService.info(`Example app listening on port ${port}`)
 })
